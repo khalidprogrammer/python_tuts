dad39f9011ac8d9d1c8e3033bc8b407a2eee9f87
khalidprogrammer <khalid.csit1234@gmail.com>
2020-10-06 23:24:07 +0430

    code push to origin

--- read_file.py
+++ read_file.py
@@ -1,12 +1,13 @@
-# f = open("khalid.txt","r")
-#
-# # content = f.read()
-#
-# # for line in f:
-# #     print(line,end="")
-#
-# print(f.readline())
-# print(f.readline())
-# print(f.readline())
+f = open("khalid.txt","r")
+print("lines was print below \n")
+
+# content = f.read()
+
+# for line in f:
+#     print(line,end="")
+
+print(f.readline())
+print(f.readline())
+print(f.readline())
 
 
